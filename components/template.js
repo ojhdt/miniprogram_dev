82c0a5f3fb470e2da3ac31d186c44eacdabaa9e2
ojhdt <ojhdtmail@gmail.com>
2022-01-22 12:53:16 +0800

    rename & create template

--- components/template.js
+++ components/template.js
@@ -5,59 +5,19 @@ Component({
    * 组件的属性列表
    */
   properties: {
-    selectedImgArr: {
-      type: Array,
-      value: []
-    },
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-    selectedImgArr: [],
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-    _previewImage(e) {
-      wx.previewImage({
-        urls: [].concat(e.target.id),
-      })
-    },
-    _deleteImage(e) {
-      if (this.data.selectedImgArr.length == 1) {
-        this.setData({
-          selectedImgArr: []
-        })
-      } else {
-        let temp = this.data.selectedImgArr.filter(n => n != e.currentTarget.dataset.id)
-        this.setData({
-          selectedImgArr: temp
-        })
-      }
-      let detail = {
-        selectedImgArr: this.data.selectedImgArr
-      }
-      this.triggerEvent("change", detail, {})
-    },
-    _chooseImage(e) {
-      wx.chooseImage({
-        count: 1,
-      }).then(res => {
-        this.setData({
-          selectedImgArr: this.data.selectedImgArr.concat(res.tempFilePaths)
-        })
-        let detail = {
-          selectedImgArr: this.data.selectedImgArr
-        }
-        this.triggerEvent("change", detail, {})
-      }).catch(err => {
-        this.triggerEvent("fail", err, {})
-      })
-    },
+    
   },
   lifetimes: {
     attached() {
